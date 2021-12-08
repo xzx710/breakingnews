676dd6893db84cec901c743ada27d1e71aaec026
xzx710 <2974758363@qq.com>
2021-12-08 08:15:06 +0800

    登录注册完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,10 +1,5 @@
 // 每次调用$.get()或$.post()或$.ajax()的时候，会先调用ajaxPreFilter这个函数
 
 $.ajaxPrefilter(function (options) {
-<<<<<<< HEAD
     options.url = 'http://localhost:8080' + options.url;
 });
-=======
-    options.url = 'http://127.0.0.1:8080' + options.url;
-})
->>>>>>> login
